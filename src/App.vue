4d81d628f8c3a3a65aad9179cf4855788934f365
Anton Plechov <bald56rus@gmail.com>
2021-02-25 21:54:28 +0400

    ДЗ "Валидация", перименование currency в coin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div
+      v-if="visibleStub"
+      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
     <div class="container">
       <div class="w-full my-4"></div>
       <section>
@@ -10,18 +35,35 @@
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
-                v-model="currency"
+                v-model="coinName"
                 type="text"
                 name="wallet"
                 id="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
+                @input="coinNameInputHandler"
+                @keypress.enter="startTrackingCoin(coinName)"
               />
+            </div>
+            <div
+              v-if="tooltips.length > 0"
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+            >
+              <span
+                v-for="(tooltip, idx) in tooltips"
+                :key="idx"
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                {{ tooltip }}
+              </span>
+            </div>
+            <div v-if="visibleAddError" class="text-sm text-red-600">
+              Такой тикер уже добавлен
             </div>
           </div>
         </div>
         <button
-          @click="startTrackCurrency"
+          @click="startTrackingCoin(coinName)"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -42,29 +84,29 @@
         </button>
       </section>
 
-      <template v-if="trackedCurrencies.length > 0">
+      <template v-if="trackedCoins.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            @click="selectedCurrency = currency"
-            v-for="currency in trackedCurrencies"
-            :key="currency.name"
+            v-for="coin in trackedCoins"
+            :key="coin.name"
+            @click="selectedCoin = coin"
             :class="{
-              'border-2': selectedCurrency === currency
+              'border-2': selectedCoin === coin
             }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ currency.name }} - USD
+                {{ coin.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ currency.price }}
+                {{ coin.price }}
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click.stop="stopTrackCurrency(currency)"
+              @click.stop="stopTrackingCoin(coin)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -85,9 +127,9 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedCurrency" class="relative">
+      <section v-if="selectedCoin" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedCurrency.name }} - USD
+          {{ selectedCoin.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
@@ -108,9 +150,9 @@
             x="0"
             y="0"
             viewBox="0 0 511.76 511.76"
-            style="enable-background:new 0 0 512 512"
+            style="enable-background: new 0 0 512 512"
             xml:space="preserve"
-            @click="selectedCurrency = null"
+            @click="selectedCoin = null"
           >
             <g>
               <path
@@ -131,50 +173,91 @@ export default {
   name: "App",
   data() {
     return {
-      currency: null,
-      trackedCurrencies: [],
-      selectedCurrency: null
+      coinName: "",
+      trackedCoins: [],
+      selectedCoin: null,
+      availableCoins: [],
+      tooltips: [],
+      visibleStub: true,
+      visibleAddError: false
     };
   },
   methods: {
-    startTrackCurrency() {
-      const trackedCurrency = {
-        name: this.currency,
+    startTrackingCoin(coinName) {
+      if (coinName === "") {
+        return;
+      }
+      try {
+        this.checkAvailableCoinTracking(coinName);
+      } catch (error) {
+        return;
+      }
+      const trackedCoin = {
+        name: coinName,
         price: "-",
         priceHistory: []
       };
-      this.trackedCurrencies.push(trackedCurrency);
-      this.currency = null;
+      this.trackedCoins.push(trackedCoin);
+      this.coinName = "";
+      this.tooltips = [];
     },
-    stopTrackCurrency(currency) {
-      this.trackedCurrencies = this.trackedCurrencies.filter(
-        trackedCurrency => trackedCurrency != currency
+    stopTrackingCoin(coin) {
+      this.trackedCoins = this.trackedCoins.filter(
+        trackedCoin => trackedCoin != coin
       );
     },
-    getCurrencyPrice(currency) {
-      let url = `https://min-api.cryptocompare.com/data/price?fsym=${currency}&tsyms=USD`;
+    getCoinPrice(coin) {
+      let url = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`;
       return fetch(url).then(response => response.json());
     },
     priceGraph() {
-      let min = Math.min(...this.selectedCurrency.priceHistory);
-      let max = Math.max(...this.selectedCurrency.priceHistory);
+      let min = Math.min(...this.selectedCoin.priceHistory);
+      let max = Math.max(...this.selectedCoin.priceHistory);
       let delta = max - min;
       min -= delta * 0.1;
       max += delta * 0.1;
-      return this.selectedCurrency.priceHistory.map(
+      return this.selectedCoin.priceHistory.map(
         price => ((price - min) * 100) / (max - min)
       );
+    },
+    getCoinList() {
+      let url = "";
+      fetch(url).then(response => console.log(response.json()));
+    },
+    coinNameInputHandler() {
+      if (this.visibleAddError) {
+        this.visibleAddError = false;
+      }
+      if (this.coinName !== "") {
+        this.tooltips = this.availableCoins
+          .filter(({ FullName, Symbol }) => {
+            return (
+              FullName.includes(this.coinName) || Symbol.includes(this.coinName)
+            );
+          })
+          .slice(-4)
+          .map(coin => coin.Symbol);
+      }
+    },
+    checkAvailableCoinTracking(coin) {
+      if (this.trackedCoins.some(({ name }) => name === coin)) {
+        this.visibleAddError = true;
+        throw new Error("Уже в списке отслеживания");
+      }
     }
   },
   created() {
+    setTimeout(() => {
+      this.visibleStub = false;
+    }, 5000);
     setInterval(() => {
-      if (this.trackedCurrencies.length > 0) {
-        this.trackedCurrencies.forEach(currency => {
-          this.getCurrencyPrice(currency.name).then(price => {
+      if (this.trackedCoins.length > 0) {
+        this.trackedCoins.forEach(coin => {
+          this.getCoinPrice(coin.name).then(price => {
             let formattedPrice =
               price.USD > 1 ? price.USD.toFixed(2) : price.USD.toPrecision(2);
-            currency.price = formattedPrice;
-            currency.priceHistory.push(price.USD);
+            coin.price = formattedPrice;
+            coin.priceHistory.push(price.USD);
           });
         });
       }
